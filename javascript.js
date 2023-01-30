c72d2b9239ea27bb849a9594fc2765ba8c5da18c
alex bocknek <alx.bock@gmail.com>
2023-01-30 15:59:40 -0800

    Created the function of getComputerChoice in the .js file that first generates a random number between 1 and 3 and converts that value into a string containing rock, paper, or scissors.Then it prints the string.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log ("Hello, world.")
+function getComputerChoice (){
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+    if (computerChoice === 1) {
+        return computerChoice = "Rock";
+        } else if (computerChoice === 2) {
+        return computerChoice = "Paper";
+        } else if (computerChoice ===3) {
+        return computerChoice = "Scissors";
+        }
+    console.log (computerChoice);
+}
