ff4eb2372c25ce8dcba835ef86498f0fb12d9b5d
alex bocknek <alx.bock@gmail.com>
2023-01-31 16:38:03 -0800

    Filled out possible outcomes in playRound function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
 function getComputerChoice (){
-    let computerSelection = Math.floor(Math.random()*3) + 1;
-    if (computerSelection === 1) {
-        return computerSelection = "Rock";
-        } else if (computerSelection === 2) {
-        return computerSelection = "Paper";
-        } else if (computerSelection ===3) {
-        return computerSelection = "Scissors";
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+    if (computerChoice === 1) {
+        return computerChoice = "Rock";
+        } else if (computerChoice === 2) {
+        return computerChoice = "Paper";
+        } else if (computerChoice ===3) {
+        return computerChoice = "Scissors";
         }
 }
 
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return roundResult = "You tied!";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return roundResult = "You lose! Paper beats rock.";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return roundResult = "You win! Rock beats scissors.";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return roundResult = "You win! Paper beats rock.";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return roundResult = "You lose! Scissors beat paper.";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return roundResult = "You lose! Rock beats scissors.";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return roundResult = "You win! Scissors beat paper.";
+    }
+}
+
+const playerSelection = "Rock";
 const computerSelection = getComputerChoice ();
+
 
