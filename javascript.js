565d01ace7a22b5760c9a4d74fb817aa2956fd3c
alex bocknek <alx.bock@gmail.com>
2023-01-31 16:18:04 -0800

    Added computerSelection as a global variable. Renamed computer choice in the getComputerChoice function has a local variable computerSelection. Removed the console.log command in function in getComputerChoice function. Created function playRound with no code.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 function getComputerChoice (){
-    let computerChoice = Math.floor(Math.random()*3) + 1;
-    if (computerChoice === 1) {
-        return computerChoice = "Rock";
-        } else if (computerChoice === 2) {
-        return computerChoice = "Paper";
-        } else if (computerChoice ===3) {
-        return computerChoice = "Scissors";
+    let computerSelection = Math.floor(Math.random()*3) + 1;
+    if (computerSelection === 1) {
+        return computerSelection = "Rock";
+        } else if (computerSelection === 2) {
+        return computerSelection = "Paper";
+        } else if (computerSelection ===3) {
+        return computerSelection = "Scissors";
         }
-    console.log (computerChoice);
 }
+
+const computerSelection = getComputerChoice ();
+
