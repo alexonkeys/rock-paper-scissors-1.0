944e0df7c8f26bc61a49c0afd6f8658f1b396d5c
alex bocknek <alx.bock@gmail.com>
2023-01-31 16:58:45 -0800

    Function playRound works as expected as long as the arguments passed to it are playerSelectiona as a  proper noun Rock, Paper, or Scissors and computerSelection is getComputerChoice ()

--- javascript.js
+++ javascript.js
@@ -1,33 +1,36 @@
 function getComputerChoice (){
-    let computerChoice = Math.floor(Math.random()*3) + 1;
-    if (computerChoice === 1) {
-        return computerChoice = "Rock";
-        } else if (computerChoice === 2) {
-        return computerChoice = "Paper";
-        } else if (computerChoice ===3) {
-        return computerChoice = "Scissors";
+    let computerSelection = Math.floor(Math.random()*3) + 1;
+    if (computerSelection === 1) {
+        return computerSelection = "Rock";
+        } else if (computerSelection === 2) {
+        return computerSelection = "Paper";
+        } else if (computerSelection ===3) {
+        return computerSelection = "Scissors";
         }
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection,computerSelection){
     if (playerSelection === computerSelection) {
-        return roundResult = "You tied!";
+        return "You tied!";
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        return roundResult = "You lose! Paper beats rock.";
+        return "You lose! Paper beats rock.";
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return roundResult = "You win! Rock beats scissors.";
+        return "You win! Rock beats scissors.";
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return roundResult = "You win! Paper beats rock.";
+        return "You win! Paper beats rock.";
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        return roundResult = "You lose! Scissors beat paper.";
+        return "You lose! Scissors beat paper.";
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return roundResult = "You lose! Rock beats scissors.";
+        return "You lose! Rock beats scissors.";
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return roundResult = "You win! Scissors beat paper.";
+        return "You win! Scissors beat paper.";
     }
 }
 
 const playerSelection = "Rock";
 const computerSelection = getComputerChoice ();
+const roundResult = playRound(playerSelection,computerSelection);
+
+
 
 
